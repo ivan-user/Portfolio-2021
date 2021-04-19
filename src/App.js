7839759609ad2263cfdf2abe35edf46a7bfd05db
Ivan Klaric <all.online.regs@gmail.com>
2021-04-19 16:05:45 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+    <div class="bg-primary">
+        <h1>Hello website</h1>
+        <h2>Hello number 2</h2>
+    </div>
+    </>
+  );
+}
+
+export default App;
